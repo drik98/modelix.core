7807527568e8e0d6b4fd74ab38251585238852d6
Jason Biatek <jbiatek@gmail.com>
2024-01-08 12:37:32 -0600

    WIP: Implementing child node access through typed Vue API

--- model-api-gen/src/main/kotlin/org/modelix/metamodel/generator/VuejsMMGenerator.kt
+++ model-api-gen/src/main/kotlin/org/modelix/metamodel/generator/VuejsMMGenerator.kt
@@ -2,10 +2,7 @@ package org.modelix.metamodel.generator
 
 import com.squareup.kotlinpoet.ClassName
 import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.json.JsonPrimitive
-import kotlinx.serialization.json.encodeToJsonElement
+import kotlinx.serialization.json.*
 import org.modelix.model.data.LanguageData
 import org.modelix.model.data.Primitive
 import org.modelix.model.data.PrimitivePropertyType
@@ -83,6 +80,7 @@ class VuejsMMGenerator(val outputDir: Path, val nameConfig: NameConfig = NameCon
             export interface IVuejsGeneratedConcept extends IConceptJS {
                 get fqName(): string;
                 features: Map<string, RoleDecl>;
+                getChildConcepts: () => Map<string, IConceptJS>;
             }
             export interface IVuejsTypedNode extends ITypedNode {
                 _node: INodeJS;
@@ -91,6 +89,230 @@ class VuejsMMGenerator(val outputDir: Path, val nameConfig: NameConfig = NameCon
                 _parent?: INodeJS;
             }
             
+            export interface IVuejsChildArray<T extends ITypedNode> extends Array<T> {
+            	length: number;
+            	[n: number]: T;
+            	createNew: (index?: number) => T;
+            	move: ((fromIndex: number, toIndex: number) => T) | ((child: T, toIndex: number) => T);
+            	delete: (child: T) => void;
+            	toArray: () => Array<T>;
+            	replaceAll: (newArray: T[]) => void;
+            }
+
+            class ChildArrayImpl<T extends ITypedNode> implements IVuejsChildArray<T> {
+            	#containingNode: INodeJS;
+            	#role: string;
+            	#concept: IConceptJS;
+            	[n: number]: T;
+            
+            	constructor(containingNode: INodeJS, role: string, concept: IConceptJS) {
+            		this.#containingNode = containingNode;
+            		this.#role = role;
+            		this.#concept = concept;
+            		// Proxies are the only mechanism for intercepting arbitrary property requests AFAIK
+            		const handler: ProxyHandler<ChildArrayImpl<T>> = {
+            			get: (target: ChildArrayImpl<T>, p: string | symbol) => {
+            				if (typeof p === "string" && /^\d+${'$'}/.test(p)) {
+            					return target.at(Number.parseInt(p));
+            				}
+            				return Reflect.get(target, p);
+            			},
+            			set: (target: ChildArrayImpl<T>, p: string | symbol, newValue: any): boolean => {
+            				if (typeof p === "string" && /^\d+${'$'}/.test(p)) {
+            					return target.set(Number.parseInt(p), newValue);
+            				}
+            				return Reflect.set(target, p, newValue);
+            			},
+            		};
+            		return new Proxy<ChildArrayImpl<T>>(this, handler);
+            	}
+            	*[Symbol.iterator]() {
+            		var yieldCount = 0;
+            		for (var i = 0; i < this.length; i++) {
+            			yieldCount++;
+            			yield this.at(i);
+            		}
+            		return yieldCount;
+            	}
+            	// This shouldn't matter for us at all, it specifies how the with() statement should behave with regard
+            	// to some Array properties, but with() isn't even allowed in strict mode anyway.
+            	[Symbol.unscopables] = Array.prototype[Symbol.unscopables]
+            
+            	at(index: number): T {
+            		return LanguageRegistry.INSTANCE.wrapNode(
+            			this.#containingNode.getChildren(this.#role)[index]
+            		) as T;
+            	}
+            	set(index: number, child: T): boolean {
+            		this.#containingNode.moveChild(this.#role, index, child.unwrap());
+            		return true;
+            	}
+            	push(...newItems: T[]): number {
+            		for (var newChild of newItems) {
+            			this.set(this.length, newChild);
+            		}
+            		return this.length;
+            	}
+            	get length(): number {
+            		return this.#containingNode.getChildren(this.#role).length;
+            	}
+            	delete(child: T): void {
+            		this.#containingNode.removeChild(child.unwrap());
+            	}
+            	move(childOrIndex: T | number, toIndex: number): T {
+            		var movingChild: INodeJS;
+            		if (typeof childOrIndex == "number") {
+            			// it's an index
+            			movingChild = this.#containingNode.getChildren(this.#role)[childOrIndex];
+            		} else {
+            			// it's a wrapped node
+            			movingChild = childOrIndex.unwrap();
+            		}
+            		this.#containingNode.moveChild(this.#role, toIndex, movingChild);
+            
+            		return LanguageRegistry.INSTANCE.wrapNode(movingChild) as T;
+            	}
+            	toArray(): Array<T> {
+            		return this.#containingNode
+            			.getChildren(this.#role)
+            			.map((child) => LanguageRegistry.INSTANCE.wrapNode(child) as T);
+            	}
+            	
+            	createNew(index: number = -1): T {
+            		const newRawNode = this.#containingNode.addNewChild(this.#role, index, this.#concept);
+            		return LanguageRegistry.INSTANCE.wrapNode(newRawNode) as T;
+            	}
+            	replaceAll(newArray: T[]): void {
+            		for (var i = 0; i < newArray.length; i++) {
+            			this.move(newArray[i], i);
+            		}
+            		for (var i = newArray.length; i < this.length; i++) {
+            			this.delete(this.at(i));
+            		}
+            	}
+            
+            	// All of the remaining methods are trivial, and allow this to be treated as a normal JavaScript Array
+            	// with all the bells and whistles. Each one just grabs the "real" method from Array.prototype and calls
+            	// it on ourselves.
+            
+            	// Using the prototypes from Array *should* be safe, as they are set up to assume nothing about the array
+            	// except that it has a proper this.length and that its elements are accessible like this[0], this[1], etc.
+            
+            	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array#generic_array_methods
+            
+            	map<U>(callbackfn: (value: T, index: number, array: T[]) => U, thisArg?: any): U[] {
+            		return Array.prototype.map.call(this, callbackfn) as U[]
+            	}
+                find<S extends T>(predicate: (value: T, index: number, obj: T[]) => value is S, thisArg?: any): S | undefined {
+            		return Array.prototype.find.call(this, predicate)
+            	}
+            	pop(): T | undefined {
+            		return Array.prototype.pop.call(this)
+            	}
+            	concat(...items: (T | ConcatArray<T>)[]): T[] {
+            		return Array.prototype.concat.call(this)
+            	}
+                join(separator?: string): string {
+            		return Array.prototype.join.call(this, separator)
+            	}
+            	reverse(): T[] {
+            		return Array.prototype.reverse.call(this)
+            	}
+            	shift(): T | undefined {
+            		return Array.prototype.shift.call(this)
+            	}
+            	slice(start?: number, end?: number): T[] {
+            		return Array.prototype.slice.call(this, start, end)
+            	}
+            	sort(compareFn?: (a: T, b: T) => number): this {
+            		Array.prototype.sort.call(this, compareFn)
+            		return this
+            	}
+            	splice(start: number, deleteCount?: number): T[] {
+            		return Array.prototype.splice.call(this, start, deleteCount)
+            	}
+            	unshift(...items: T[]): number {
+            		return Array.prototype.unshift.call(this, items)
+            	}
+            	indexOf(searchElement: T, fromIndex?: number): number {
+            		return Array.prototype.indexOf.call(this, searchElement, fromIndex)
+            	}
+            	lastIndexOf(searchElement: T, fromIndex?: number): number {
+            		return Array.prototype.lastIndexOf.call(this, searchElement, fromIndex)
+            	}
+            	every<S extends T>(predicate: (value: T, index: number, array: T[]) => value is S, thisArg?: any): this is S[] {
+            		return Array.prototype.every.call(this, predicate, thisArg)
+            	}
+            	some(predicate: (value: T, index: number, array: T[]) => unknown, thisArg?: any): boolean {
+            		return Array.prototype.some.call(this, predicate)
+            	}
+            	forEach(callbackfn: (value: T, index: number, array: T[]) => void, thisArg?: any): void {
+            		Array.prototype.forEach.call(this, callbackfn)
+            	}
+            	filter<S extends T>(predicate: (value: T, index: number, array: T[]) => value is S, thisArg?: any): S[] {
+            		return Array.prototype.filter.call(this, predicate)
+            	}
+            	// I'm not going to go back and change the above Array.prototypes, but at this point I got stuck trying
+            	// to make TypeScript happy with some of the more complex type definitions. It's not actually necessary
+            	// to do these with proper types, though, since it's all going to be untyped at runtime anyway, and the
+            	// enforcement of the interface is in `interface Array<T>`, not here. This should only cause issues if
+            	// someone re-types this object as ChildArrayImpl directly and then passes in incompatible stuff.
+            	reduce(callbackfn: any, initialValue?: any): any {
+            		return Array.prototype.reduce.call(this, callbackfn, initialValue)
+            	}
+            	reduceRight(callbackfn: any, initialValue?: any): any {
+            		return Array.prototype.reduceRight.call(this, callbackfn, initialValue)
+            	}
+            	findIndex(predicate: any, thisArg?: any): number {
+            		return Array.prototype.findIndex.call(this, predicate, thisArg)
+            	}
+            	fill(value: T, start?: number, end?: number): this {
+            		Array.prototype.fill.call(this, value, start, end)
+            		return this
+            	}
+            	copyWithin(target: number, start: number, end?: number): this {
+            		Array.prototype.copyWithin.call(this, target, start, end)
+            		return this
+            	}
+            	entries(): IterableIterator<[number, T]> {
+            		return Array.prototype.entries.call(this)
+            	}
+            	keys(): IterableIterator<number> {
+            		return Array.prototype.keys.call(this)
+            	}
+            	values(): IterableIterator<T> {
+            		return Array.prototype.values.call(this)
+            	}
+            	includes(searchElement: T, fromIndex?: number): boolean {
+            		return Array.prototype.includes.call(this, searchElement, fromIndex)
+            	}
+            	flatMap( callback: any, thisArg?: any): any[] {
+            		return Array.prototype.flatMap.call(this, callback, thisArg)
+            	}
+            	flat(thisArg: any, depth?: any) : any[] {
+            		return Array.prototype.flat.call(thisArg, depth)
+            	}
+                findLast(predicate: (value: T, index: number, array: T[]) => unknown, thisArg?: any): T | undefined {
+            		return Array.prototype.findLast.call(this, predicate, thisArg)
+            	}
+            	findLastIndex(predicate: (value: T, index: number, array: T[]) => unknown, thisArg?: any): number {
+            		return Array.prototype.findLastIndex.call(this, predicate, thisArg)
+            	}
+            	toReversed(): T[] {
+            		return Array.prototype.toReversed.call(this)
+            	}
+            	toSorted(compareFn?: (a: T, b: T) => number): T[] {
+            		return Array.prototype.toSorted.call(this, compareFn)
+            	}
+            	toSpliced(...args: any[]): any[] {
+            		// @ts-expect-error Types on this one are a bit odd too. Just take whatever we're given and pass it along.
+            		return Array.prototype.toSpliced.apply(this, [...args])
+            		// Technically, they could all just be done this way.
+            	}
+            	with(index: number, value: T): T[] {
+            		return Array.prototype.with.call(this, index, value)
+            	}
+            }            
             type INodeReferenceJS = any;
             const wrappedNodeCache = new Map<INodeReferenceJS, IVuejsTypedNode>();
             // https://stackoverflow.com/a/68488123
@@ -168,31 +390,46 @@ class VuejsMMGenerator(val outputDir: Path, val nameConfig: NameConfig = NameCon
                     if (existingComputedRef !== undefined) {
                         return existingComputedRef;
                     }
-                    const newComputedRef = customRef<ITypedNode | ITypedNode[]>(
-                        (track, trigger) => {
-                            triggers.set(role, trigger);
-                            return {
-                                get: () => {
-                                    track();
-                                    const childArray = node
-                                        .getChildren(role)
-                                        .map((child) => LanguageRegistry.INSTANCE.wrapNode(child));
-                                    if (feature.multiple) {
-                                        // Object.freeze(childArray);
-                                        return childArray;
-                                    } else {
-                                        // return childArray;
-                                        return childArray[0];
+		            if (feature.multiple) {
+		            	// Contains multiple children for this role, so use our array-like wrapper.
+		            	const newComputedRef = customRef<ChildArrayImpl<ITypedNode>>((track, trigger) => {
+		            		triggers.set(role, trigger);
+		            		const childArrayWrapper = new ChildArrayImpl<ITypedNode>(node, role, concept)
+		            		return {
+		            			get: () => {
+		            				track();
+		            				return childArrayWrapper;
+		            			},
+		            			set: (_value) => {
+		            				childArrayWrapper.replaceAll(_value.toArray())
+		            			},
+		            		};
+		            	});
+		            	refs.set(role, newComputedRef);
+		            	return newComputedRef;
+		            } else {
+		            	// Only one child in this role, so this is pretty easy
+		            	const newComputedRef = customRef<ITypedNode>((track, trigger) => {
+		            		triggers.set(role, trigger);
+		            		return {
+		            			get: () => {
+		            				track();
+		            				const childArray = node
+		            					.getChildren(role)
+		            					.map((child) => LanguageRegistry.INSTANCE.wrapNode(child));
+		            				return childArray[0];
+		            			},
+		            			set: (_value) => {
+		            				node.removeChild(node.getChildren(role)[0])
+                                    if (_value != null) {
+		            				    node.moveChild(role, 0, _value.unwrap())
                                     }
-                                },
-                                set: (_value) => {
-                                    throw "can not set child";
-                                },
-                            };
-                        }
-                    );
-                    refs.set(role, newComputedRef);
-                    return newComputedRef;
+		            			},
+		            		};
+		            	});
+		            	refs.set(role, newComputedRef);
+		            	return newComputedRef;
+		            }
                 };
                 const getComputedRefForRef = (role: string, feature: RoleDecl) => {
                     const existingComputedRef = refs.get(role);
@@ -249,6 +486,14 @@ class VuejsMMGenerator(val outputDir: Path, val nameConfig: NameConfig = NameCon
             				}
                         }
                         const key = keyOrSymbol as string;
+	            		if (/^__new_child_/.test(key)) {
+	            			// Special synthetic method for instantiating a non-multiple child. (The multiple version of this
+	            			// is in ChildArrayImpl.)
+	            			const role = key.replace("__new_child_", "")
+	            			const childConcept = concept.getChildConcepts().get(role)
+	            			_node.addNewChild(role, 0, childConcept)
+	            			return getComputedRefForChild(role, concept.features.get(role) as ChildLinkDecl).value
+	            		}
                         const feature = concept.features.get(key);
                         if (!feature) return undefined;
                         switch (feature.kind) {
@@ -322,7 +567,7 @@ class VuejsMMGenerator(val outputDir: Path, val nameConfig: NameConfig = NameCon
             } from "@modelix/ts-model-api";
             
             import { computed, WritableComputedRef, shallowReactive, Ref, unref, toRaw, triggerRef } from "vue";
-            import { IVuejsGeneratedConcept, IVuejsTypedNode, wrapNode, RoleDecl } from "./index";
+            import { IVuejsGeneratedConcept, IVuejsTypedNode, IVuejsChildArray, wrapNode, RoleDecl } from "./index";
             
             function triggerRefValue(ref: Ref, value: any) {
                 return triggerRef(ref)
@@ -389,9 +634,16 @@ class VuejsMMGenerator(val outputDir: Path, val nameConfig: NameConfig = NameCon
                 }
                 is ProcessedChildLink -> {
                     val N_InterfaceName = feature.type.resolved.tsInterfaceRef(concept.language)
-                    """
-                        ${feature.generatedName}: ${if (feature.multiple) "Array<$N_InterfaceName>" else N_InterfaceName }
-                    """.trimIndent()
+                    if (feature.multiple) {
+                        """
+                        ${feature.generatedName}: IVuejsChildArray<$N_InterfaceName>
+                        """.trimIndent()
+                    } else {
+                        """
+                        ${feature.generatedName}: $N_InterfaceName;
+                        __new_child_${feature.generatedName}: () => $N_InterfaceName;
+                        """.trimIndent()
+                    }
                 }
                 else -> ""
             }
@@ -419,6 +671,12 @@ class VuejsMMGenerator(val outputDir: Path, val nameConfig: NameConfig = NameCon
               }
               getDirectSuperConcepts(): Array<IConceptJS> {
                 return [${concept.getDirectSuperConcepts().joinToString(",") { it.languagePrefix(concept.language) + it.conceptWrapperInterfaceName() }}];
+              }
+              getChildConcepts(): Map<string, IConceptJS> {
+                const theMap = new Map<string, IConceptJS>()
+                ${concept.getAllSuperConcepts().flatMap { it.getOwnRoles() }.filterIsInstance<ProcessedChildLink>()
+                        .joinToString("\n") { "theMap.set(\"${it.generatedName}\", ${it.concept.languagePrefix(concept.language) + it.concept.conceptWrapperInterfaceName()})" }}
+                return theMap
               }
               get fqName() {
                 return "${concept.fqName()}";
