61947303a241472b5d0ad6a5a88aa1474cf9117e
Hendrik Schmitz <hendrikschmitz306@gmail.com>
2024-04-23 11:43:21 +0200

    Improve isOfConcept_ typeguards to differentiate between val and refs

--- model-api-gen/src/main/kotlin/org/modelix/metamodel/generator/VuejsMMGenerator.kt
+++ model-api-gen/src/main/kotlin/org/modelix/metamodel/generator/VuejsMMGenerator.kt
@@ -420,7 +420,7 @@ class VuejsMMGenerator(val outputDir: Path, val nameConfig: NameConfig = NameCon
                 ${features}
             }
             
-            export function isOfConcept_${concept.name}(node: MaybeRef<IVuejsTypedNode | null | undefined>): node is ${concept.nodeWrapperInterfaceName()} {
+            export function isOfConcept_${concept.name}(node: MaybeRef<IVuejsTypedNode | null | undefined>): node is MaybeRef<${concept.nodeWrapperInterfaceName()}> {
                 return ${concept.conceptWrapperInterfaceName()} === toRaw(unref(node).?_concept);
             }
             
